b0fd4777cf89125f67e385adb153fa68cfb31de9
Pajar-Padillah <81399270+Pajar-Padillah@users.noreply.github.com>
2023-08-21 16:17:20 +0700

    first commit

---
+++ css/javascript.js
@@ -0,0 +1,282 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+
+
+// Monitoring UK/TK pada unit beka
+    
+function myFunction0() {
+    document.getElementById("myDropdown0").classList.toggle("show");
+  }
+  
+  function filterFunction0() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown0");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
+  
+
+// laporanproduksi harian //
+{
+    function myFunction1() {
+        document.getElementById("myDropdown1").classList.toggle("show");
+      }
+      
+      function filterFunction1() {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("myInput1");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("myDropdown1");
+        a = div.getElementsByTagName("a");
+        for (i = 0; i < a.length; i++) {
+          txtValue = a[i].textContent || a[i].innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+          } else {
+            a[i].style.display = "none";
+          }
+        }
+      }
+}
+
+// laporan manajemen unit
+{
+    function myFunction2() {
+        document.getElementById("myDropdown2").classList.toggle("show");
+      }
+      
+      function filterFunction2() {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("myInput2");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("myDropdown2");
+        a = div.getElementsByTagName("a");
+        for (i = 0; i < a.length; i++) {
+          txtValue = a[i].textContent || a[i].innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+          } else {
+            a[i].style.display = "none";
+          }
+        }
+      }
+}
+
+
+// monitoring uk/tk pada unit Suli
+{
+    function myFunction3() {
+        document.getElementById("myDropdown3").classList.toggle("show");
+      }
+      
+      function filterFunction3() {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("myInput3");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("myDropdown3");
+        a = div.getElementsByTagName("a");
+        for (i = 0; i < a.length; i++) {
+          txtValue = a[i].textContent || a[i].innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+          } else {
+            a[i].style.display = "none";
+          }
+        }
+      }
+}
+
+
+// Monev PBJ 
+{
+    function myFunction4() {
+        document.getElementById("myDropdown4").classList.toggle("show");
+      }
+      
+      function filterFunction4() {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("myInput4");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("myDropdown4");
+        a = div.getElementsByTagName("a");
+        for (i = 0; i < a.length; i++) {
+          txtValue = a[i].textContent || a[i].innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+          } else {
+            a[i].style.display = "none";
+          }
+        }
+      }
+}
+//5
+{
+    function myFunction5() {
+        document.getElementById("myDropdown5").classList.toggle("show");
+      }
+      
+      function filterFunction5() {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("myInput5");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("myDropdown5");
+        a = div.getElementsByTagName("a");
+        for (i = 0; i < a.length; i++) {
+          txtValue = a[i].textContent || a[i].innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+          } else {
+            a[i].style.display = "none";
+          }
+        }
+      }
+}
+
+
+// MONITORING DROPPING BAHAN KIMIA & ALAT PANEN
+{
+    function myFunction6() {
+        document.getElementById("myDropdown6").classList.toggle("show");
+      }
+      
+      function filterFunction6() {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("myInput6");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("myDropdown6");
+        a = div.getElementsByTagName("a");
+        for (i = 0; i < a.length; i++) {
+          txtValue = a[i].textContent || a[i].innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+          } else {
+            a[i].style.display = "none";
+          }
+        }
+      }
+}
+
+
+//MONITORING HAMA & PENYAKIT TANAMAN
+
+{
+    function myFunction7() {
+        document.getElementById("myDropdown7").classList.toggle("show");
+      }
+      
+      function filterFunction7() {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("myInput7");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("myDropdown7");
+        a = div.getElementsByTagName("a");
+        for (i = 0; i < a.length; i++) {
+          txtValue = a[i].textContent || a[i].innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+          } else {
+            a[i].style.display = "none";
+          }
+        }
+      }
+}
+
+// monitoring pemupukan
+{
+    function myFunction8() {
+        document.getElementById("myDropdown8").classList.toggle("show");
+      }
+      
+      function filterFunction8() {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("myInput8");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("myDropdown8");
+        a = div.getElementsByTagName("a");
+        for (i = 0; i < a.length; i++) {
+          txtValue = a[i].textContent || a[i].innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+          } else {
+            a[i].style.display = "none";
+          }
+        }
+      }
+}
+
+// KELENGKAPAN ADMINISTRASI
+{
+    function myFunction9() {
+        document.getElementById("myDropdown9").classList.toggle("show");
+      }
+      
+      function filterFunction9() {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("myInput9");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("myDropdown9");
+        a = div.getElementsByTagName("a");
+        for (i = 0; i < a.length; i++) {
+          txtValue = a[i].textContent || a[i].innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+          } else {
+            a[i].style.display = "none";
+          }
+        }
+      }
+}
+//10
+{
+    function myFunction10() {
+        document.getElementById("myDropdown10").classList.toggle("show");
+      }
+      
+      function filterFunction10() {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("myInput10");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("myDropdown10");
+        a = div.getElementsByTagName("a");
+        for (i = 0; i < a.length; i++) {
+          txtValue = a[i].textContent || a[i].innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+          } else {
+            a[i].style.display = "none";
+          }
+        }
+      }
+}
+
+// 90
+{
+  function myFunction90() {
+      document.getElementById("myDropdown90").classList.toggle("show");
+    }
+    
+    function filterFunction90() {
+      var input, filter, ul, li, a, i;
+      input = document.getElementById("myInput90");
+      filter = input.value.toUpperCase();
+      div = document.getElementById("myDropdown90");
+      a = div.getElementsByTagName("a");
+      for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          a[i].style.display = "";
+        } else {
+          a[i].style.display = "none";
+        }
+      }
+    }
+}
